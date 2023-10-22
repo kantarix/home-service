b7ef31254c94079209627bd58adb2d0fcb7405ef
Flethy <petrova.vera.ev@gmail.com>
2023-10-22 20:18:21 +0300

    imports fixes

--- src/main/kotlin/com/kantarix/home_service/api/controllers/HomeController.kt
+++ src/main/kotlin/com/kantarix/home_service/api/controllers/HomeController.kt
@@ -4,9 +4,15 @@ import com.kantarix.home_service.api.dto.Home
 import com.kantarix.home_service.api.dto.HomeSimple
 import com.kantarix.home_service.api.dto.request.HomeRequest
 import com.kantarix.home_service.api.services.HomeService
-import com.kantarix.home_service.store.entities.HomeEntity
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/homes")
